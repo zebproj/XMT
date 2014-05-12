3d1a053ac598a4d261c071c42eebc0d70035fd8f
Paul Batchelor <pbatchelor@berklee.edu>
2014-05-12 10:37:48 -0400

    added basic note generation

--- xmt.c
+++ xmt.c
@@ -5,21 +5,84 @@
 
 #define AMIGA 0x0
 #define LINEAR 0x1
-
+#define NOTE 0b1
+#define INSTRUMENT 0b10
+#define VOLUME 0b100
+#define FX 0b1000
+#define PARAM 0b10000
 typedef struct
 {
-int size;
-uint8_t *data;
+	uint8_t pscheme;
+	uint8_t note;
+	uint8_t instrument;
+	uint8_t volume;
+	uint8_t fx;
+	uint8_t fx_param;
 }
-pat_data;
+xm_note;
+
+xm_note make_note(
+		uint8_t note,
+		uint8_t ins,
+		uint8_t vol,
+		uint8_t fx,
+		uint8_t param)
+{
+	xm_note n;
+	n.pscheme = 0x80;
+	if(note != 0){
+		n.pscheme = n.pscheme | NOTE;
+		n.note = note;
+	}
+	if(ins != 0){
+		n.pscheme = n.pscheme | INSTRUMENT;
+		n.instrument = ins;
+	}
+	if(vol != 0){
+		n.pscheme = n.pscheme | VOLUME;
+		n.volume = vol;
+	}
+	if(fx != 0){
+		n.pscheme = n.pscheme | FX;
+		n.fx = fx;
+	}
+	if(param != 0){
+		n.pscheme = n.pscheme | PARAM;
+		n.fx_param = param;
+	}
+
+	return n;
+}
+
+void init_note(xm_note *n)
+{
+	
+}
+
+void write_note(FILE *f, xm_note *n)
+{
+	fwrite(&n->pscheme, sizeof(uint8_t), 1, f);
+	if(n->pscheme & NOTE)
+		fwrite(&n->note, sizeof(uint8_t), 1, f);
+	if(n->pscheme & INSTRUMENT)
+		fwrite(&n->instrument, sizeof(uint8_t), 1, f);
+	if(n->pscheme & VOLUME)
+		fwrite(&n->volume, sizeof(uint8_t), 1, f);
+	if(n->pscheme & FX)
+		fwrite(&n->fx, sizeof(uint8_t), 1, f);
+	if(n->pscheme & PARAM)
+		fwrite(&n->fx_param, sizeof(uint8_t), 1, f);
+}
 
 typedef struct
 {
 	uint32_t header_size;
 	uint8_t packing_type;
 	uint16_t num_rows; 
+	uint16_t num_channels;
 	uint16_t data_size;
-	uint8_t *data;
+	//uint8_t *data;
+	xm_note *data;
 }
 xm_pat;
 
@@ -57,14 +120,66 @@ typedef struct
 }
 xm_file;
 
-void init_xm_pat(xm_file *f, xm_pat *p)
+void add_note(	xm_file *f, 
+				uint8_t patnum,
+				uint8_t chan,
+				uint8_t row,
+				xm_note note)
+{	
+	if((patnum + 1)> f->num_patterns) f->num_patterns = (patnum + 1);
+	xm_pat *p = &f->pat[patnum];
+	/* limit varibles */
+	//row = row % (p->num_rows);
+	if(note.pscheme & NOTE) p->data_size++;
+	if(note.pscheme & INSTRUMENT) p->data_size++;
+	if(note.pscheme & VOLUME) p->data_size++;
+	if(note.pscheme & FX) p->data_size++;
+	if(note.pscheme & PARAM) p->data_size++;
+	p->data[(row * p->num_channels) + chan ] = note;
+
+}
+
+void set_BPM(xm_file *f, uint8_t bpm) 
 {
-	p->header_size = 0x09; 
-	p->packing_type = 0x00;
-	p->num_rows = 0x40;
-	p->data_size = p->num_rows * f->num_channels;
-	p->data = (uint8_t *)malloc(sizeof(uint8_t) * p->data_size);
-	memset(p->data, 0x80, sizeof(uint8_t) * p->data_size);
+	f->BPM =  bpm;
+}
+//fix this implementation
+void set_nchan(xm_file *f, uint8_t n)
+{
+	f->num_channels = 1<<n;
+}
+void set_speed(xm_file *f, uint8_t speed) 
+{	
+	f->speed = speed;
+}
+
+void remove_note(	xm_pat *p, 
+					uint8_t row,
+					uint8_t chan)
+{
+
+}
+
+void init_xm_pat(xm_file *f)
+{
+	int i, j;
+
+	for(j = 0; j < 256; j++)
+	{
+		xm_pat *p = &f->pat[j];
+		p->header_size = 0x09; 
+		p->packing_type = 0x00;
+		p->num_rows = 0x40;
+		p->num_channels = f->num_channels;
+		p->data_size = p->num_rows * f->num_channels;
+		//p->data = (uint8_t *)malloc(sizeof(uint8_t) * p->data_size);
+		//memset(p->data, 0x80, sizeof(uint8_t) * p->data_size);
+		p->data = (xm_note *)malloc(sizeof(xm_note) * p->data_size);
+		for(i = 0; i < p->data_size; i++) 
+		{
+			p->data[i].pscheme = 0x80;
+		}
+	}
 }
 
 void init_xm_ins(xm_file *f, xm_ins *i)
@@ -96,20 +211,34 @@ void init_xm_file(xm_file *f){
 	f->BPM = 125;
 	//initialize pattern table
 	memset(f->ptable, 0x0, sizeof(uint8_t) * 256);
-	f->ptable[0] = 1;	
-	init_xm_pat(f, &f->pat[0]);
+	init_xm_pat(f);
 	init_xm_ins(f, &f->ins[0]);
+
 }
 
+void update_ptable(xm_file *f, uint8_t pos, uint8_t pnum){
+	if((pos + 1) > f->song_length) f->song_length = pos + 1;
+	f->ptable[pos] = pnum;	
+}
+int add_pattern(xm_file *f){
+	return f->num_patterns++;
+}
 void write_pattern_data(xm_file *f)
 {
-	int i;
-	fwrite(&f->pat[0].header_size, sizeof(uint32_t), 1, f->file);
-	fwrite(&f->pat[0].packing_type, sizeof(uint8_t), 1, f->file);
-	fwrite(&f->pat[0].num_rows, sizeof(uint16_t), 1, f->file);
-	fwrite(&f->pat[0].data_size, sizeof(uint16_t), 1, f->file);
-	fwrite(f->pat[0].data, sizeof(uint8_t), f->pat[0].data_size, f->file);
+	int i, p;
+	
+	for(p = 0; p < f->num_patterns; p++)
+	{
+		fwrite(&f->pat[p].header_size, sizeof(uint32_t), 1, f->file);
+		fwrite(&f->pat[p].packing_type, sizeof(uint8_t), 1, f->file);
+		fwrite(&f->pat[p].num_rows, sizeof(uint16_t), 1, f->file);
+		fwrite(&f->pat[p].data_size, sizeof(uint16_t), 1, f->file);
+		for(i = 0; i < f->pat[p].num_rows * f->num_channels; i++){
+			write_note(f->file, &f->pat[p].data[i]);
+		}
+	}
 }
+
 
 void write_instrument_data(xm_file *f)
 {
@@ -149,6 +278,17 @@ int main()
 	xm_file file;
 
 	init_xm_file(&file);
+
+	update_ptable(&file, 1, 1);
+	/*users shouldn't be able to set the channel numbers after initialization*/
+	//set_nchan(&file, 2);
+	add_note(&file, 1, 0, 0, make_note(60, 1, 0, 0, 0));
+	add_note(	
+			&file, 
+			0,
+			0,
+			1,
+			make_note(62, 0, 20, 0, 0));
 
 	write_xm_file(&file);
 
